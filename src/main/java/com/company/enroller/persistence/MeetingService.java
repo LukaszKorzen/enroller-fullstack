b12053664aff002d1dc8bd2444b5785e5d511991
Lukasz <korzen1984@gmail.com>
2021-07-01 01:39:14 +0200

    try

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -3,12 +3,17 @@ package com.company.enroller.persistence;
 import com.company.enroller.model.Meeting;
 import org.hibernate.Query;
 import org.springframework.stereotype.Component;
-
+import com.company.enroller.model.Participant;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import java.util.Collection;
 
 @Component("meetingService")
 public class MeetingService {
 
+	
+	Session session;
+	
     DatabaseConnector connector;
 
     public MeetingService() {
@@ -19,6 +24,43 @@ public class MeetingService {
         String hql = "FROM Meeting";
         Query query = connector.getSession().createQuery(hql);
         return query.list();
+    }
+    public Meeting findById(long id) {
+        return (Meeting) session.get(Meeting.class, id);
+    }
+
+    public void add(Meeting meeting) {
+        Transaction transaction = this.session.beginTransaction();
+        session.save(meeting);
+        transaction.commit();
+    }
+
+    public void delete(Meeting meeting) {
+        Transaction transaction = this.session.beginTransaction();
+        session.delete(meeting);
+        transaction.commit();
+    }
+
+    public void update(Meeting meeting) {
+        Transaction transaction = this.session.beginTransaction();
+        session.merge(meeting);
+        transaction.commit();
+    }
+
+    public Participant addParticipant(Meeting meeting, Participant participant) {
+        Transaction transaction = this.session.beginTransaction();
+        meeting.addParticipant(participant);
+        session.merge(meeting);
+        transaction.commit();
+        return participant;
+    }
+
+    public Meeting deleteParticipant(Meeting meeting, Participant participant) {
+        Transaction transaction = this.session.beginTransaction();
+        meeting.removeParticipant(participant);
+        session.merge(meeting);
+        transaction.commit();
+        return meeting;
     }
 
 
